ace87a0c9a89599a9e4e4e8d0065ec2687a408c7
Durga <60353753+Durga944@users.noreply.github.com>
2021-10-12 06:25:00 +0530

    Add files via upload

---
+++ dd.js
@@ -0,0 +1,23 @@
+// var arr = [..."Durga",..."Dharwey"];
+// console.log(arr);
+
+// var arr = ["Durga","Dharwey"];
+// var i = 0;
+// var l = [];
+// while(i<arr.length){
+//     var j = 0;
+//     while(j<arr[i].length){
+//         l.push(arr[i][j]);
+//         j++
+//     }
+//     i++
+// }
+// console.log(l);
+
+// function sum(){
+//     console.log("Durga");
+// }
+// sum();
+// console.log(typeof(sum));
+var a = Date.now();
+console.log(a);
